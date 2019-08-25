0a2d24f080e78e176f1e1bb701c7382feb1a5fc5
Patrick Legislador <patlegislador@gmail.com>
2019-08-24 23:12:03 -0400

    Add highlight on click and scroll - 80% progress

--- script.js
+++ script.js
@@ -5,53 +5,52 @@ collapseBtn.addEventListener('click', function() {
     navBar.classList.toggle('show');
 });
 
-function scroll(target, duration) {
-    var target = document.querySelector(target);
-    var position = target.getBoundingClientRect().top;
-    var start = window.pageYOffset;
-    var distance = position - start;
-    var startTime = null;
+const collapselinks_node = document.querySelectorAll('.collapselink');
+const collapselinks = Array.from(collapselinks_node);
 
-    function scrollAnimation(timestamp) {
-        if(startTime === null) {
-            startTime = timestamp;
-        }
-        var timeElapsed = timestamp - startTime;
-        var exe = Math.easeInOutQuad(timeElapsed, start, distance, position);
-        window.scrollTo(0, exe);
-        if(timeElapsed < duration) {
-            requestAnimationFrame(scrollAnimation);
-        }
-    }
+collapselinks.forEach(e => {
+    e.addEventListener('click', function() {
+        navBar.classList.remove('show');
+    });
+})
 
-    Math.easeInOutQuad = function (t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
-        t--;
-        return -c/2 * (t*(t-2) - 1) + b;
-    };
-    
+/** HIGHLIGHT ON CLICK **/
 
-    requestAnimationFrame(scrollAnimation);
+const navlinks = Array.from(document.querySelectorAll('.section'));
+
+navlinks.forEach(el => el.addEventListener('click', e => setActive(el)));
+
+
+function setActive(el) {
+    [...el.parentElement.children].forEach(sib => sib.classList.remove('active'))
+    el.classList.add('active')
 }
 
-// const homepage = document.querySelector('a[href="#home"]');
-// const aboutus = document.querySelector('a[href="#aboutus"]');
-// const services = document.querySelector('a[href="#services"]');
-// const contact = document.querySelector('a[href="#contact"]');
+/** HIGHLIGHT ON SCROLL **/
 
-// homepage.addEventListener('click', function() {
-//     scroll('#home', 1000);
-// });
 
-// aboutus.addEventListener('click', function() {
-//     scroll('#aboutus', 1000);
-// });
+const sections = Array.from(document.querySelectorAll('section'));
 
-// services.addEventListener('click', function() {
-//     scroll('#services', 1000);
-// });
+sections.forEach(function(el) {
+  el.addEventListener('mouseenter', function() {
+    var anchors = Array.from(document.querySelectorAll('a'));
+    anchors.forEach(function(el) {
+      el.classList.remove('active');
+    })
+  
+    var menu = document.querySelector("a[href='#" + this.id + "']");
+    menu.classList.add('active');
+    console.log(menu);
+  });
+});
 
-// contact.addEventListener('click', function() {
-//    scroll('#contact', 1000); 
-// });
+function setActiveAuto(el) {
+  var anchors = Array.from(document.querySelectorAll('a'));
+  anchors.forEach(function(el) {
+    el.classList.remove('active');
+  })
+
+  var menu = document.querySelector("a[href='#" + el.id + "']");
+  menu.classList.add('active');
+  console.log(menu);
+}
